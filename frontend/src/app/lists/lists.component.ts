609cbf47811d7f310cccfb77bb877c70539edbfa
Adam Ross <adamross202@gmail.com>
2024-08-03 00:28:26 -0600

    recipe collections front end

--- frontend/src/app/lists/lists.component.ts
+++ frontend/src/app/lists/lists.component.ts
@@ -1,12 +1,56 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-lists',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './lists.component.html',
-  styleUrl: './lists.component.scss'
+  styleUrl: './lists.component.scss',
 })
 export class ListsComponent {
+  protected lists$ = of([
+    {
+      listId: 1,
+      listName: 'Juices',
+      recipeId: 1,
+    },
+    {
+      listId: 1,
+      listName: 'Main Dishes',
+      recipeId: 1,
+    },
+    {
+      listId: 1,
+      listName: 'Cheap Meals',
+      recipeId: 1,
+    },
+    {
+      listId: 1,
+      listName: 'Quick Meals',
+      recipeId: 1,
+    },
+    {
+      listId: 1,
+      listName: 'Treats',
+      recipeId: 1,
+    },
+    {
+      listId: 1,
+      listName: 'Bread',
+      recipeId: 1,
+    },
+    {
+      listId: 1,
+      listName: 'Quick Meals',
+      recipeId: 1,
+    },
+    {
+      listId: 1,
+      listName: 'Treats',
+      recipeId: 1,
+    },
 
+  ]);
 }
