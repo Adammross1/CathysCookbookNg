972f1d64811f43f5ce6173814b31650168e3b231
Adam Ross <adamross202@gmail.com>
2024-08-20 10:29:18 -0600

    can now download shopping list as a pdf or email it to yourself

--- backend/obj/Debug/net8.0/CathysCookbookAPI.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/CathysCookbookAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CathysCookbookAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+220668c35d5a3a8aa88aec16d3f83ed5d0cf1093")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+059e940aac8e34dbc7d00b202620516b62b93541")]
 [assembly: System.Reflection.AssemblyProductAttribute("CathysCookbookAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CathysCookbookAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- frontend/src/app/meal-prep/meal-prep.component.ts
+++ frontend/src/app/meal-prep/meal-prep.component.ts
@@ -1,22 +1,31 @@
-import { Component, inject } from '@angular/core';
+import { Component, ElementRef, inject, ViewChild } from '@angular/core';
 import { CcRecipesService } from '../core/services/cc-recipes.service';
 import { CommonModule } from '@angular/common';
-import { combineLatest, map, Observable } from 'rxjs';
+import { combineLatest, map } from 'rxjs';
 import { Ingredient, Recipe, RecipeDetail } from '../core/models/recipe';
 import { SelectedRecipesService } from '../core/services/selected-recipes.service';
 import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import emailjs from 'emailjs-com';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-meal-prep',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './meal-prep.component.html',
   styleUrl: './meal-prep.component.scss',
 })
 export class MealPrepComponent {
+  @ViewChild('shoppingListTable', { static: false })
+  shoppingListTable!: ElementRef;
   private ccRecipesService = inject(CcRecipesService);
   private selectedRecipesService = inject(SelectedRecipesService);
   protected route = inject(ActivatedRoute);
+  protected http = inject(HttpClient);
+  protected email: string = '';
   protected searchFilter = '';
   onInputChange(event: Event) {
     this.ccRecipesService.setSearchRecipeFilterSubject(
@@ -43,6 +52,7 @@ export class MealPrepComponent {
   protected selectedRecipes$ =
     this.selectedRecipesService.getSelectedRecipesSubjectAsObservable();
 
+  private shoppingListString = '';
   protected shoppingList$ = this.selectedRecipesService
     .getSelectedRecipesSubjectAsObservable()
     .pipe(
@@ -63,6 +73,12 @@ export class MealPrepComponent {
             }
           });
         });
+        this.shoppingListString = shoppingList
+          .map(
+            (ingredient) =>
+              `${ingredient.ingredientName} ${ingredient.amount} ${ingredient.measurementName}`
+          )
+          .join('\n');
         return shoppingList;
       })
     );
@@ -80,5 +96,58 @@ export class MealPrepComponent {
 
   private deselectRecipe(recipe: Recipe) {
     this.selectedRecipesService.removeRecipeFromSelectedRecipes(recipe);
+  }
+
+  sendEmail() {
+    // Send the email using EmailJS
+    emailjs
+      .send(
+        'service_upmfo5q',
+        'template_59xvt0o',
+        {
+          to_name: this.email,
+          to_email: this.email,
+          message: this.shoppingListString,
+        },
+        'Pq3KTtKAldCwhZ467'
+      )
+      .then((response) => {
+        console.log('Email sent successfully:', response);
+      })
+      .catch((error) => {
+        console.error('Error sending email:', error);
+      });
+  }
+
+  generatePDF() {
+    const tableElement = this.shoppingListTable.nativeElement;
+
+    html2canvas(tableElement).then((canvas) => {
+      const imgData = canvas.toDataURL('image/png');
+      const pdf = new jsPDF({
+        orientation: 'p', // 'p' for portrait, 'l' for landscape
+        unit: 'mm',
+        format: 'a4',
+      });
+
+      const imgWidth = 210; // A4 paper width in mm
+      const pageHeight = 295; // A4 paper height in mm
+      const imgHeight = (canvas.height * imgWidth) / canvas.width;
+      let heightLeft = imgHeight;
+
+      let position = 0;
+
+      pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+      heightLeft -= pageHeight;
+
+      while (heightLeft >= 0) {
+        position = heightLeft - imgHeight;
+        pdf.addPage();
+        pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+        heightLeft -= pageHeight;
+      }
+
+      pdf.save('shopping-list.pdf');
+    });
   }
 }
